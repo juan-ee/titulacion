80e90be20e804ab9bbeeb8ab33e3939367a3f89a
juan-ee <juanque@outlook.com>
2019-01-01 01:15:31 -0500

    rerfactor some things

--- apis.js
+++ apis.js
@@ -1,115 +1,114 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 module.exports = {
-    getPois: function (location,radius,type,callback) {
-        request(
-            {
-                uri:'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-                qs: {
-                    location: `${location.lat},${location.lng}`,
-                    radius:radius,
-                    type:type,
-                    name:type,
-                    // key:'AIzaSyD4qbkCAmjG4kObzEXT9gnzPGHel3Tuk44',
-                    key:'AIzaSyB8-GllalpxSUubV2qgh0rhba-wsKE5t3c'
-                },
-                json: true
-            }).then(function (resp) {
-                    callback(resp.results);
-            });
-    },
-    getDetails: function (placeid,callback) {
-        request(
-            {
-                uri:'https://maps.googleapis.com/maps/api/place/details/json',
-                qs: {
-                    placeid: placeid,
-                    // key:'AIzaSyD4qbkCAmjG4kObzEXT9gnzPGHel3Tuk44',
-                    key:'AIzaSyB8-GllalpxSUubV2qgh0rhba-wsKE5t3c'
-                },
-                json: true
-            }).then(function (resp) {
-                var poi = resp.result;
-                poi.location = poi.geometry.location;
-                delete poi.geometry;
-                delete poi.address_components;
-                delete poi.adr_address;
-                delete poi.photos;
-                delete poi.reference;
-                delete poi.reviews;
-                delete poi.scope;
-                delete poi.types;
-                poi.expected_time = getExpectedTime();
-                //adjust opening hours
-                try {
-                    const periods = poi.opening_hours.periods;
-                    var newPeriods = new Array(7);
-                    for (var i in periods){
-                        newPeriods[periods[i].open.day] = {
-                            open:periods[i].open.time,
-                            close:periods[i].close.time,
-                        }
-                    }
-                    poi.opening_hours.periods = newPeriods;
-                }catch (err){}
-                callback(poi);
-            });
-    },
-    getTimeMatrix: function (pois,key,callback) {
-        var matrix = [new Array(pois.length)];
-        var promises = [];
-        for(var i=0; i<pois.length-1; i++){
-            matrix.push(new Array(pois.length));
-            for(var j=i+1; j<pois.length; j++){
-                promises.push(new Promise(function (resolve) {
-                        getRoute(key,i === 0 ? `${pois[i].location.lat},${pois[i].location.lng}` :`place_id:${pois[i].place_id}`,`place_id:${pois[j].place_id}`,function (route) {
-                            resolve(route);
-                        });
-                    })
-                );
-            }
+  getPois: (location, radius, type, callback) => {
+    request({
+      uri: process.env.GOOGLE_PLACES_URL,
+      qs: {
+        location: `${location.lat},${location.lng}`,
+        radius: radius,
+        type: type,
+        name: type,
+        key: process.env.GOOGLE_API_KEY
+      },
+      json: true
+    }).then(resp => {
+      callback(resp.results);
+    });
+  },
+  getDetails: (placeid, callback) => {
+    request({
+      uri: process.env.GOOGLE_PLACES_DETAILS_URL,
+      qs: {
+        placeid: placeid,
+        key: process.env.GOOGLE_API_KEY
+      },
+      json: true
+    }).then(resp => {
+      var poi = resp.result;
+      poi.location = poi.geometry.location;
+      delete poi.geometry;
+      delete poi.address_components;
+      delete poi.adr_address;
+      delete poi.photos;
+      delete poi.reference;
+      delete poi.reviews;
+      delete poi.scope;
+      delete poi.types;
+      poi.expected_time = getExpectedTime();
+      //adjust opening hours
+      try {
+        const periods = poi.opening_hours.periods;
+        var newPeriods = new Array(7);
+        for (var i in periods) {
+          newPeriods[periods[i].open.day] = {
+            open: periods[i].open.time,
+            close: periods[i].close.time
+          };
         }
-        Promise.all(promises).then(function (values) {
-           var ind = 0;
-           for(var i=0; i<pois.length-1; i++){
-                for(var j=i+1; j<pois.length; j++){
-                    matrix[i][j]=values[ind];
-                    matrix[j][i]=values[ind];
-                    ind++;
-                }
-            }
-            callback(matrix);
-        });
+        poi.opening_hours.periods = newPeriods;
+      } catch (err) {}
+      callback(poi);
+    });
+  },
+  getTimeMatrix: function(pois, key, callback) {
+    var matrix = [new Array(pois.length)];
+    var promises = [];
+    for (var i = 0; i < pois.length - 1; i++) {
+      matrix.push(new Array(pois.length));
+      for (var j = i + 1; j < pois.length; j++) {
+        promises.push(
+          new Promise(resolve => {
+            getRoute(
+              key,
+              i === 0
+                ? `${pois[i].location.lat},${pois[i].location.lng}`
+                : `place_id:${pois[i].place_id}`,
+              `place_id:${pois[j].place_id}`,
+              route => {
+                resolve(route);
+              }
+            );
+          })
+        );
+      }
     }
+    Promise.all(promises).then(values => {
+      var ind = 0;
+      for (var i = 0; i < pois.length - 1; i++) {
+        for (var j = i + 1; j < pois.length; j++) {
+          matrix[i][j] = values[ind];
+          matrix[j][i] = values[ind];
+          ind++;
+        }
+      }
+      callback(matrix);
+    });
+  }
 };
 
-function getRoute(key,origin,destination,callback){
-    request(
-        {
-            uri:'https://maps.googleapis.com/maps/api/directions/json',
-            qs: {
-                origin: origin,
-                destination:destination,
-                units:'metric',
-                mode:'walking',
-                key:key
-                // key:'AIzaSyBjLWzGBsWZIBwBNVMCqXbjwFEzNfomR0k'
-                // key:'AIzaSyAgg8nvSsVsJo_fOvJB0113sJ9saV6BgEo'
-                // key:'AIzaSyDZJ4eK77EVxoJsUIzqUZMJgpmxJsFQvyo'
-                // key:'AIzaSyCfqVM0MdOINSwxu_n9Sy_nYzs29-La-zE'
-            },
-            json: true
-        }).then(function (resp) {
-            const route=resp.routes[0];
-            obj = {
-                // distance:route.legs[0].distance.value,
-                duration:route.legs[0].duration.value / 60,
-                points:route.overview_polyline.points
-            };
+function getRoute(key, origin, destination, callback) {
+  request({
+    uri: process.env.GOOGLE_DIRECTIONS_URL,
+    qs: {
+      origin: origin,
+      destination: destination,
+      units: "metric",
+      mode: "walking",
+      key: key
+    },
+    json: true
+  }).then(resp => {
+    const route = resp.routes[0];
+    obj = {
+      // distance:route.legs[0].distance.value, TODO find out why this
+      duration: route.legs[0].duration.value / 60,
+      points: route.overview_polyline.points
+    };
 
-        callback(obj);
-    });
+    callback(obj);
+  });
 }
 function getExpectedTime() {
-    return Math.ceil(Math.random()*60+30);
+  return Math.ceil(Math.random() * 60 + 30);
 }
--- app.js
+++ app.js
@@ -1,183 +1,217 @@
-const ga = require('ga-tsp');
-const KmeansLib = require('kmeans-same-size');
-const express = require('express');
-const app =  express();
-const api = require('./apis');
+const ga = require("ga-tsp");
+const KmeansLib = require("kmeans-same-size");
+const kmeans = new KmeansLib();
+const express = require("express");
+const app = express();
+const api = require("./apis");
+const CircularJSON = require('circular-json');
+require("dotenv").config();
 // const bodyParser = require('body-parser');
 // const fs = require('fs');
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // app.use(bodyParser.json()); // support json encoded bodies
 // app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(8080);
 
-app.get('/pois',function (req,res) {
-    const userInfo = req.query;
-    var pois = [];
-    var promises = [];
-    userInfo.location = {
-        lat:parseFloat(userInfo.location.lat),
-        lng:parseFloat(userInfo.location.lng)
-    };
-    for(var i in userInfo.categories){
-        promises.push(
-            new Promise(function (resolve) {
-                api.getPois(userInfo.location,2000,userInfo.categories[i],function (results) {
-                    resolve(results);
-                });
-            })
+app.get("/pois", (req, res) => {
+  const userInfo = req.query;
+  var pois = [];
+  var promises = [];
+  userInfo.location = {
+    lat: parseFloat(userInfo.location.lat),
+    lng: parseFloat(userInfo.location.lng)
+  };
+  for (var i in userInfo.categories) {
+    promises.push(
+      new Promise(resolve => {
+        api.getPois(
+          userInfo.location,
+          2000,
+          userInfo.categories[i],
+          results => {
+            resolve(results);
+          }
         );
-    }
-    Promise.all(promises).then(function (results) {
-        pois = pois.concat.apply(pois, results);
-        //---------RESOLVER ESTO--------
-        pois.sort(function(a, b) {
-            return b.rating - a.rating;
+      })
+    );
+  }
+  Promise.all(promises).then(results => {
+    pois = pois.concat.apply(pois, results);
+    //---------RESOLVER ESTO--------
+    pois.sort((a, b) => b.rating - a.rating);
+    pois = pois.slice(0, userInfo.days * 6);
+    //-------------------------------
+    getDetails(pois, pois_details => {
+      if (pois_details.length < userInfo.days) {
+        res.json("Not enough points of interest for your preferences.");
+        return;
+      }
+      getClusters(pois_details, userInfo.days, clusters => {
+        getTours(clusters, userInfo.location, tours => {
+          getRestaurants(tours, restaurants => {
+            res.render("map", { array: tours, restaurants: restaurants });
+          });
         });
-        pois = pois.slice(0,userInfo.days*6);
-        //-------------------------------
-        getDetails(pois,function (pois_details) {
-            if(pois_details.length<userInfo.days){res.json('Not enough points of interest for your preferences.');return;}
-            getClusters(pois_details,userInfo.days,function (clusters) {
-                getTours(clusters,userInfo.location,function (tours) {
-                    getRestaurants(tours,function (restaurants) {
-                        res.render('map',{array:tours,restaurants:restaurants});
-                    })
-                })
-            });
-        });
+      });
     });
+  });
 });
 
-
-function getDetails(pois,callback){
-    var promises=[];
-    for(var i in pois){
-        if (pois[i].rating !== undefined){
-            promises.push(
-                new Promise(function (resolve) {
-                    api.getDetails(pois[i].place_id,function (result) {
-                        resolve(result);
-                    });
-                })
-            );
-        }
+function getDetails(pois, callback) {
+  var promises = [];
+  for (var i in pois) {
+    if (pois[i].rating !== undefined) {
+      promises.push(
+        new Promise(resolve => {
+          api.getDetails(pois[i].place_id, result => {
+            resolve(result);
+          });
+        })
+      );
     }
-    Promise.all(promises).then(function (pois) {
-        callback(pois);
-    });
+  }
+  Promise.all(promises).then(pois => {
+    callback(pois);
+  });
 }
 
-function getClusters(pois,days,callback) { //ordenar de menor a mayor PILAS, de acuerdo al tiempo
-    var kmeans =  new KmeansLib();
-    var clusters=new Array(days);
+function getClusters(pois, days, callback) {
+  //ordenar de menor a mayor PILAS, de acuerdo al tiempo
+  const clusters = new Array(days);
 
-    // ver luego lo de Location
-    pois.map(function (poi) {
-        poi.lat = poi.location.lat;
-        poi.lng = poi.location.lng;
-        return poi;
-    });
+  // ver luego lo de Location
+  pois = pois.map(poi => ({...poi,
+    x: poi.location.lat,
+    y: poi.location.lng
+  }));
 
-    kmeans.init({k: days, runs: ~~(pois.length / days), equalSize: true, normalize: false });
-    kmeans.calc(pois);
+  kmeans.init({
+    k: days,
+    runs: ~~(pois.length / days),
+    equalSize: true,
+    normalize: false
+  });
+  kmeans.calc(pois);
 
-    // group in clusters
-    for(i in pois){
-        var index=pois[i].k;
-        delete pois[i].k;
-        try{
-            clusters[index].push(pois[i]);
-        }catch(err){
-            clusters[index]=[];
-            clusters[index].push(pois[i]);
-        }
+  // group in clusters
+  for (let poi of pois) {
+    const index = poi.k;
+    delete poi.k;
+    delete poi.x;
+    delete poi.y;
+    try {
+      clusters[index].push(poi);
+    } catch (_) {
+      clusters[index] = [];
+      clusters[index].push(poi);
     }
-    callback(clusters);
+  }
+  callback(clusters);
 }
 
+function getTours(clusters, userLocation, callback) {
+  var tours = [];
+  var promises = [];
+  var n = -1;
+  const keys = [
+    "AIzaSyAgg8nvSsVsJo_fOvJB0113sJ9saV6BgEo",
+    "AIzaSyDZJ4eK77EVxoJsUIzqUZMJgpmxJsFQvyo",
+    "AIzaSyCfqVM0MdOINSwxu_n9Sy_nYzs29-La-zE",
+    "AIzaSyBjLWzGBsWZIBwBNVMCqXbjwFEzNfomR0k"
+  ];
 
+  clusters.forEach((cluster, index, clusters) => {
+    n++;
+    n = n === keys.length ? 0 : n;
 
-function getTours(clusters,userLocation,callback) {
-    var tours = [];
-    var promises = [];
-    var n = -1;
-    const keys = ['AIzaSyAgg8nvSsVsJo_fOvJB0113sJ9saV6BgEo','AIzaSyDZJ4eK77EVxoJsUIzqUZMJgpmxJsFQvyo','AIzaSyCfqVM0MdOINSwxu_n9Sy_nYzs29-La-zE','AIzaSyBjLWzGBsWZIBwBNVMCqXbjwFEzNfomR0k'];
-
-    clusters.forEach(function (cluster,index,clusters) {
-        n++;
-        n = n === keys.length ? 0 : n ;
-
-        //--------RESOLVER ESTO LUEGO---------
-        // clusters[index] = cluster.slice(0,6);
-        clusters[index].unshift({location:userLocation});
-        //------------------------------------
-        promises.push(new Promise(function (resolve) {
-            api.getTimeMatrix(clusters[index],keys[n],function (matrix) {
-                resolve(matrix);
-            });
-        }));
-    });
-
-    Promise.all(promises).then(function (matrixes) {
-        //quitar esto
-        // fs.appendFileSync('matrixes.txt', `\n--SOLICITUD--`);
-        // fs.appendFileSync('matrixes.txt', `\n--clusters-\n${JSON.stringify(clusters)}\n-------\n`);
-        // fs.appendFileSync('matrixes.txt', `\n--matrixes--\n${JSON.stringify(matrixes)}\n-------\n`);
-
-        // ------------resolver de acuerdo a las fechas escogidas-------------
-        var date = new Date(new Date().toDateString().split(' ').splice(1,3).join(' ')+' 10:00:00');
-        //----------------------------------------------------------------
-        for(var i in matrixes){
-            date.setDate(date.getDate()+1);
-            const parameters = {
-                pois:clusters[i],
-                date:date,
-                timeMatrix:matrixes[i],
-                mutation_rate: 0.03,
-                sizePopulation:clusters[i].length*clusters[i].length,
-                totalGenerations:10*clusters[i].length
-            };
-            tours.push(ga.evolve(parameters));
-        }
-        callback(tours);
-    });
-}
-
-function getRestaurants(tours,callback){
-    promises = [];
-    tours.forEach(function (tour) {
-        tour.forEach(function (value,index,array) {
-            if(value.lunch){
-                promises.push(new Promise(function (resolve) {
-                        api.getPois(value.location,100,'restaurant',function (result) {
-                            if (result.length > 0){
-                                result.map(function (restaurant) {
-                                    restaurant.time = `${value.start} - ${value.end}`;
-                                    return restaurant;
-                                });
-                                resolve(result.splice(0,5));
-                            }else{
-                                api.getPois(array[array.indexOf(value)-2].poi.location,100,'restaurant',function (result) {
-                                    result.map(function (restaurant) {
-                                        restaurant.time = `${value.start} - ${value.end}`;
-                                        return restaurant;
-                                    });
-                                    resolve(result.splice(0,5));
-                                });
-                            }
-                        });
-                    }
-                ));
-            }
+    //--------RESOLVER ESTO LUEGO---------
+    // clusters[index] = cluster.slice(0,6);
+    clusters[index].unshift({ location: userLocation });
+    //------------------------------------
+    promises.push(
+      new Promise(resolve => {
+        api.getTimeMatrix(clusters[index], process.env.API_KEY_MATRIX, matrix => {
+          resolve(matrix);
         });
+      })
+    );
+  });
+
+  Promise.all(promises).then(matrixes => {
+    //quitar esto
+    // fs.appendFileSync('matrixes.txt', `\n--SOLICITUD--`);
+    // fs.appendFileSync('matrixes.txt', `\n--clusters-\n${JSON.stringify(clusters)}\n-------\n`);
+    // fs.appendFileSync('matrixes.txt', `\n--matrixes--\n${JSON.stringify(matrixes)}\n-------\n`);
+
+    // ------------resolver de acuerdo a las fechas escogidas-------------
+    var date = new Date(
+      new Date()
+        .toDateString()
+        .split(" ")
+        .splice(1, 3)
+        .join(" ") + " 10:00:00"
+    );
+    //----------------------------------------------------------------
+    for (var i in matrixes) {
+      // TODO: pilas, ver si la puta MATRIZ se construye bien
+      // console.log('[POIS]', CircularJSON.stringify(clusters[i]));
+      // console.log('[MATRIX]', CircularJSON.stringify(matrixes[i]));
+      date.setDate(date.getDate() + 1);
+      const parameters = {
+        pois: clusters[i],
+        date: date,
+        timeMatrix: matrixes[i],
+        mutation_rate: 0.03,
+        sizePopulation: clusters[i].length * clusters[i].length,
+        totalGenerations: 10 * clusters[i].length
+      };
+      tours.push(ga.evolve(parameters));
+    }
+    callback(tours);
+  });
+}
+
+function getRestaurants(tours, callback) {
+  promises = [];
+  tours.forEach(tour => {
+    tour.forEach((value, index, array) => {
+      if (value.lunch) {
+        promises.push(
+          new Promise(resolve => {
+            api.getPois(value.location, 100, "restaurant", result => {
+              if (result.length > 0) {
+                result.map(restaurant => {
+                  restaurant.time = `${value.start} - ${value.end}`;
+                  return restaurant;
+                });
+                resolve(result.splice(0, 5));
+              } else {
+                api.getPois(
+                  array[array.indexOf(value) - 2].poi.location,
+                  100,
+                  "restaurant",
+                  result => {
+                    result.map(restaurant => {
+                      restaurant.time = `${value.start} - ${value.end}`;
+                      return restaurant;
+                    });
+                    resolve(result.splice(0, 5));
+                  }
+                );
+              }
+            });
+          })
+        );
+      }
     });
-    Promise.all(promises).then(function (restaurants) {
-        callback([].concat.apply([],restaurants));
-    });
+  });
+  Promise.all(promises).then(restaurants => {
+    callback([].concat.apply([], restaurants));
+  });
 }
 
 // app.get('/params', function (req, res) { //query params
@@ -203,5 +237,4 @@ function getRestaurants(tours,callback){
 //     const userInfo = req.query;
 //     res.json(userInfo);
 // });
-
 
